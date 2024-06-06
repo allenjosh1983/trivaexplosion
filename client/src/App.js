8e4b29595685da73811ffa81454fdbab992bf7f5
allenjosh1983 <138170235+allenjosh1983@users.noreply.github.com>
2024-06-06 12:46:25 -0700

    made all button uniform

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import QuestionForm from './components/QuestionForm';
